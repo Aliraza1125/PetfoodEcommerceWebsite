808e4fc15f4785a48618be6f9bb706a2420432e3
Ali Raza <alirazaasif2511@gmail.com>
2024-09-03 01:04:15 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import "./responsive.css"
-import './meanmenu.min.css'
+import "./responsive.css";
+import "./meanmenu.min.css";
 
-import Home from "./pages/home/Home.jsx";
-import Header from "./components/header/header";
-import Footer from "./components/footer/footer";
+// Make sure these imports have the correct paths and file names
+import Home from "./pages/home/Home";  // Check the actual casing and file path
+import Header from "./components/header/Header"; // Check the casing
+import Footer from "./components/footer/Footer"; // Check the casing
 import Login from "./pages/login/Login";
 import Food from "./pages/food/Food";
-import ProdunctDetails from "./pages/productDetails/ProdunctDetails";
+import ProductDetails from "./pages/productDetails/ProductDetails"; // Fixed typo
 import AboutUs from "./pages/aboutUs/AboutUs";
 import Blog from "./pages/blog/Blog";
 import BlogDetails from "./pages/blogDetails/BlogDetails";
-import ContactUs from "./pages/constactUs/ContactUs";
+import ContactUs from "./pages/contactUs/ContactUs"; // Fixed typo
 
 function App() {
   return (
@@ -25,14 +26,13 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/petfood" element={<Food />} />
-          <Route path="/product-details" element={<ProdunctDetails />} />
+          <Route path="/product-details" element={<ProductDetails />} /> {/* Fixed typo */}
           <Route path="/about-us" element={<AboutUs />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog-details" element={<BlogDetails />} />
-          <Route path="/contact-us" element={<ContactUs />} />
-    
+          <Route path="/contact-us" element={<ContactUs />} /> {/* Fixed typo */}
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </>
   );
